54f67a318f6d1aa1cffc5248d39fb81928d8b431
Numan Bin Nasrullah <numanbinnasrullah@gmail.com>
2024-08-19 16:20:05 +0500

    35 change

--- src/app/(components)/Homepage/hero-banner/HeroBanner.jsx
+++ src/app/(components)/Homepage/hero-banner/HeroBanner.jsx
@@ -1,27 +1,22 @@
 import Image from "next/image"
 import Link from "next/link"
-import Head from "next/head";
+
 
 const HeroBanner = () => {
     return (
         <>
-        <Head>
-        <link rel="preload" href="/Desktop.png" as="image" />
-      </Head>
-            <div className="relative w-full h-[70vh] md:h-[90vh]">
+            <div className="relative w-full ">
                 <Link href={"/about"}>
-                <div className="relative hidden sm:block w-full h-full">
                     <Image
                         src="/Desktop.png" // Replace with your image path
                         alt="Homepage Banner"
                         objectFit="contain"
                         className=" hidden sm:block "
-                       
+                        quality={85}
                         layout="intrinsic"
                         priority
-                        quality={85}
+                        loading="lazy"
                     />
-                    </div>
                 </Link>
                 <Link href={"/about"}>
                     <Image
@@ -31,8 +26,7 @@ const HeroBanner = () => {
                         objectPosition="center"
                         className=" block sm:hidden"
                         layout="intrinsic"
-                        width={1000}
-                        height={100}
+                        quality={85}
                         loading="lazy"
                     />
                 </Link>
