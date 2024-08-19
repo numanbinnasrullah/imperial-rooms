57c63af78651471062189910a8d16a2f1b8eab6f
Numan Bin Nasrullah <numanbinnasrullah@gmail.com>
2024-08-19 15:13:18 +0500

    6 change

--- src/app/(components)/Footer/Footer.jsx
+++ src/app/(components)/Footer/Footer.jsx
@@ -11,7 +11,31 @@ import Footerbar from "./Footerbar";
 const Footer = () => {
     return (
         <>
-        <h1>Footer</h1>
+            <div className="bg-[#F2F2F2] w-full px-9 py-10  xl:px-44 xl:py-14 xl:space-x-4 lg:px-8 lg:py-10 flex flex-wrap justify-center"> 
+                <div className="lg:w-[30%] md:w-[50%] leading-6 ">
+                    <h2 className="text-lg font-semibold mb-6">GET IN TOUCH</h2>
+                    <p>+44 20 850 77 44</p>
+                    <p>info@imperialrooms.co.uk</p>
+                    <p>Imperial Homeware London LTD. 269 Welington Road, Unit 1, Handsworth, Birmingham B20 2 QQ</p>
+                </div>
+                <div className="lg:w-[20%] md:w-[50%] w-full">
+                    <h2 className="text-lg font-semibold mb-6">Usefull Information</h2>
+                    <div className="flex flex-col text-sm leading-7">
+                        <Link href='/about'>Blog</Link>
+                        <Link href='/about'>About Us</Link>
+                        <Link href='/about'>Privecy Policy</Link>
+                        <Link href='/about'>Shipping Information</Link>
+                        <Link href='/about'>Security Information</Link>
+                        <Link href='/about'>Sitemap</Link>
+                        <Link href='/about'>Offers</Link>
+                    </div>
+                   
+                </div>
+             
+             
+               
+            </div>
+        <Footerbar />
         </>
     )
 }
