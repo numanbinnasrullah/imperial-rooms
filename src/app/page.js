96c7317c518147e28515f932ce2f615ca778651b
Numan Bin Nasrullah <numanbinnasrullah@gmail.com>
2024-08-26 13:07:52 +0500

    dz

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import Hero from "./(components)/Homepage/hero-banner/Hero";
 import HeroVideo from "./(components)/Homepage/hero-video/HeroVideo";
 // import NewArrival from "./(components)/Homepage/new-arrival/NewArrival";
 // import Reviews from "./(components)/Homepage/reviews/Reviews";
-// import ShopByCategory from "./(components)/Homepage/Shop-By-Category/ShopByCategory";
+import ShopByCategory from "./(components)/Homepage/Shop-By-Category/ShopByCategory";
 // import Testimonial from "./(components)/Homepage/testimonial/Testimonial";
 // import { TopSellingProducts } from "./(components)/Homepage/top-selling-products/TopSellingProducts";
 // import UserSignup from "./(components)/Homepage/user-signup/UserSignup";
@@ -16,14 +16,17 @@ import HeroVideo from "./(components)/Homepage/hero-video/HeroVideo";
 
 
 export default async function Home() {
+  const homePageData = await homePageQuery()
+  const {collection1, collection2, collection3, collection4} = homePageData?.data
+ 
   const isVideoShow = false;
   return (
     
     <>
       <h1>yes</h1>
       {isVideoShow ? <HeroVideo /> : <Hero />}
-      {/* <ShopByCategory collections={[collection1, collection2, collection3, collection4]} />
-      <NewArrival newArrival_1 = {Home_Collection_New_Arrivals_1} newArrival_2 = {Home_Collection_New_Arrivals_2} newArrival_3 =  {Home_Collection_New_Arrivals_3} /> */}
+      <ShopByCategory collections={[collection1, collection2, collection3, collection4]} />
+      {/* <NewArrival newArrival_1 = {Home_Collection_New_Arrivals_1} newArrival_2 = {Home_Collection_New_Arrivals_2} newArrival_3 =  {Home_Collection_New_Arrivals_3} /> */}
       {/* <NewArrival1 newArrival_1 = {Home_Collection_New_Arrivals_1} newArrival_2 = {Home_Collection_New_Arrivals_2} newArrival_3 =  {Home_Collection_New_Arrivals_3} /> */}
       {/* <Reviews />
       <Testimonial /> */}
