8cd2be36575372b8d10d2d785f353323ed922c47
Numan Bin Nasrullah <numanbinnasrullah@gmail.com>
2024-08-28 12:48:50 +0500

    sx

--- src/app/(components)/Homepage/hero-banner/Hero.jsx
+++ src/app/(components)/Homepage/hero-banner/Hero.jsx
@@ -1,55 +1,21 @@
-'use client'
-import { useEffect, useState } from 'react';
-import './HeroBanner.css';
 
+import './HeroBanner.css'
 const Hero = () => {
-  const [isDesktopLoaded, setDesktopLoaded] = useState(false);
-  const [isMobileLoaded, setMobileLoaded] = useState(false);
-
-  useEffect(() => {
-    const desktopImg = new Image();
-    const mobileImg = new Image();
-
-    desktopImg.src = '/Desktop.png';
-    mobileImg.src = '/mobile.png';
-
-    desktopImg.onload = () => setDesktopLoaded(true);
-    mobileImg.onload = () => setMobileLoaded(true);
-  }, []);
-
   return (
     <>
-      <div>
-        <a href=''>
-          <div className="desktop-banner-img" id="Banner-desk">
-            {!isDesktopLoaded && <div className="skeleton-loader"></div>}
-            {isDesktopLoaded && (
-              <div
-                style={{
-                  backgroundImage: `url('/Desktop.png')`,
-                }}
-              ></div>
-            )}
-          </div>
-        </a>
-      </div>
+         <div>
+      <a href=''>
+        <div class="desktop-banner-img" id="Banner-desk" data-blink-loader></div>
+      </a>
+    </div>
 
-      <div>
-        <a href=''>
-          <div className="mobile-banner-img" id="Banner-mob">
-            {!isMobileLoaded && <div className="skeleton-loader"></div>}
-            {isMobileLoaded && (
-              <div
-                style={{
-                  backgroundImage: `url('/mobile.png')`,
-                }}
-              ></div>
-            )}
-          </div>
-        </a>
-      </div>
+    <div>
+      <a href=''>
+        <div class="mobile-banner-img" id="Banner-mob" data-blink-loader></div>
+      </a>
+    </div>
     </>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
