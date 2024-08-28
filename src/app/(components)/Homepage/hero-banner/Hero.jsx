61229a4d3dadde62dad72a1efec3bf76218e8675
Numan Bin Nasrullah <numanbinnasrullah@gmail.com>
2024-08-28 16:10:38 +0500

    wer

--- src/app/(components)/Homepage/hero-banner/Hero.jsx
+++ src/app/(components)/Homepage/hero-banner/Hero.jsx
@@ -4,11 +4,21 @@ import './HeroBanner.css'
 import Heroskeleton from './skeleton';
 const Hero = () => {
   const [showBanner, setShowBanner] = useState(false);
+  const [fixedHeight, setFixedHeight] = useState(true);
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowBanner(true);
     }, 1000); // 2 seconds delay
+
+    return () => clearTimeout(timer); // Cleanup the timer on component unmount
+  }, []);
+
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setFixedHeight(false);
+    }, 10000); // 10 seconds
 
     return () => clearTimeout(timer); // Cleanup the timer on component unmount
   }, []);
@@ -18,7 +28,7 @@ const Hero = () => {
       <Heroskeleton />
     ) : (
       <>
-        <div>
+        <div style={{height: fixedHeight ? '700px' : 'auto',}}>
           <a href=''>
             <div className="desktop-banner-img" id="Banner-desk" data-blink-loader></div>
           </a>
