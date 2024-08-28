6d4e5c29a6e2f8ec2c4e33ff2db2ace1d808f086
Numan Bin Nasrullah <numanbinnasrullah@gmail.com>
2024-08-28 16:02:56 +0500

    sx

--- src/app/(components)/Header/logo-icons/LogoIcons.jsx
+++ src/app/(components)/Header/logo-icons/LogoIcons.jsx
@@ -20,7 +20,7 @@ const LogoIcons = ({sidebarOpen, toggleSidebar,searchsidebarOpen, toggleSearchba
     useEffect(() => {
         const timer = setTimeout(() => {
           setShowBanner(false);
-        }, 4000); // 2 seconds delay
+        }, 2000); // 2 seconds delay
     
         return () => clearTimeout(timer); // Cleanup the timer on component unmount
       }, []);
