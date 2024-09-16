bf9bd4f3e8b7d7452fbafe01cc1077c75867009b
Numan Bin Nasrullah <numanbinnasrullah@gmail.com>
2024-09-16 10:01:27 +0500

    banner

--- src/app/(components)/Homepage/hero-banner/Hero.jsx
+++ src/app/(components)/Homepage/hero-banner/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setShowBanner(true);
-    }, 2000); // 2 seconds delay
+    }, 1500); // 2 seconds delay
 
     return () => clearTimeout(timer); // Cleanup the timer on component unmount
   }, []);
